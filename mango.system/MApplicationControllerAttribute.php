decd6e0f735e5571bcab3462e6de31dfb21e234a
Jader Feijo <jader@movinpixel.com>
2013-10-06 16:04:54 +0100

    Added documentation for the following file: 	modified:   mango.system/MApplicationControllerAttribute.php

--- mango.system/MApplicationControllerAttribute.php
+++ mango.system/MApplicationControllerAttribute.php
@@ -31,6 +31,10 @@
 	package('mango.system');
 	
 	/**
+	 * Abstract class which forms the base for all application controller attributes
+	 *
+	 * @see MApplicationControllerField
+	 * @see MApplicationControllerParameter
 	 *
 	 * @author Jader Feijo <jader@movinpixel.com>
 	 *
@@ -52,6 +56,26 @@
 		protected $type;
 		protected $required;
 		
+		/**
+		 * This method should be overriden by subclasses of this class to perform
+		 * extra initialization as necessary
+		 *
+		 * This method initializes a new instance of a subclass of MApplicationControllerAttribute
+		 * with the specified name, type and required parameters
+		 *
+		 * @param MString $name The name of this attribute
+		 * @param int $type A constant representing the type of this attribute
+		 * Possible values are:
+		 * 	StringType
+		 *  IntegerType
+		 *  FloatType
+		 *  BooleanType
+		 *  DateType
+		 *  BinaryType
+		 *  ArrayType
+		 *  DictionaryType
+		 * @param bool $required Wether or not this attribute is required
+		 */
 		public function __construct(MString $name, $type, $required = true) {
 			MAssertTypes('int', $type, 'bool', $required);
 			parent::__construct();
@@ -64,21 +88,38 @@
 		/******************** Properties ********************/
 		
 		/**
-		 * @return MString
+		 * Returns the name of this attribute
+		 *
+		 * @return MString A string containing the name of this attribute
 		 */
 		public function name() {
 			return $this->name;
 		}
 		
 		/**
-		 * @return int
+		 * Retuns an constant representing the type of this attribute
+		 * Possible values are:
+		 *  StringType
+		 *  IntegerType
+		 *  FloatType
+		 *  BooleanType
+		 *  DateType
+		 *  BinaryType
+		 *  ArrayType
+		 *  DictionaryType
+		 *
+		 * @return int A constant representing the type of this attribute
 		 */
 		public function type() {
 			return $this->type;
 		}
 		
 		/**
-		 * @return bool
+		 * Returns a boolean which indicates wether this attribute is required
+		 * or not
+		 *
+		 * @return bool A boolean indicating wether this attribute is required
+		 * or not
 		 */
 		public function required() {
 			return $this->required;
@@ -87,7 +128,9 @@
 		/******************** Methods ********************/
 		
 		/**
-		 * @return MString
+		 * Returns a string containing the expected data type for this attribute
+		 *
+		 * @return MString A string containing the expected data type for this attribute
 		 */
 		public function expectedDataType() {
 			if ($this->type() == MApplicationControllerField::StringType) {
